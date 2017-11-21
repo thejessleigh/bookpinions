a8496da6d078caa4956ff069625586cfc6cd7267
thejessleigh <j.l.unrein@gmail.com>
2017-11-20 23:08:48 -0600

    eliminante supurflous autoincrement primary key for users.

--- bookpinions/users/models.py
+++ bookpinions/users/models.py
@@ -1,22 +1,27 @@
-from app import db
+from database import db
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    goodreads_user_id = db.Column(db.Integer, nullable=False)
+    goodreads_user_id = db.Column(db.Integer, nullable=False, primary_key=True)
     name = db.Column(db.String(255))
+    username = db.Column(db.String(255))
     created_time = db.Column(db.DateTime)
     visibility = db.Column(db.String(10), default="private")
+    access_token = db.Column(db.String(255))
+    access_secret = db.Column(db.String(255))
 
-    def __init__(self, goodreads_user_id, name, created_time):
+    def __init__(self, goodreads_user_id, name, created_time, username, access_token, access_secret):
         self.goodreads_user_id = goodreads_user_id
         self.created_time = created_time
         self.name = name
+        self.username = username
+        self.access_token = access_token
+        self.access_secret = access_secret
 
     def __repr__(self):
-        return "<User id: {id}, Name: {name}>".format(
-            id=self.id,
-            name=self.name
+        return "<Username: {username}>".format(
+            name=self.name,
+            username=self.username
         )
